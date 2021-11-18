7a92f2b5e5be7edf2672c0db053438a8e040f3ed
bayram <68708205+byrmylmz@users.noreply.github.com>
2021-11-18 15:11:35 +0300

    Authcontroller login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,34 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function login(Request $request)
+    {
+        $http = new \GuzzleHttp\Client;
+
+        try {
+            $response = $http->post('http://api.alakod.com/oauth/token', [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => 2,
+                    'client_secret' => 'gbcoHDWSLNIPZEuKHQ0DMbodMBw9VaDAVERjmbkd',
+                    'username' => $request->username,
+                    'password' => $request->password,
+                ]
+            ]);
+
+            return $response->getBody();
+
+        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+            if ($e->getCode() === 400) {
+                return response()->json('Invalid Request. Please enter a username or a password.', $e->getCode());
+            } else if ($e->getCode() === 401) {
+                return response()->json('Your credentials are incorrect. Please try again', $e->getCode());
+            }
+
+            return response()->json('Something went wrong on the server.', $e->getCode());
+        }
+    }
+
     public function register(Request $request)
     {
         $request->validate([
