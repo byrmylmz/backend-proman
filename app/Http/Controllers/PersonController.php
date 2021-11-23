7e5ec2999cb0d755392d1e1d86e6cdb8ebd6fe94
bayram <68708205+byrmylmz@users.noreply.github.com>
2021-11-23 15:43:46 +0300

    Person Controller

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $person=Person::all();
+        return $person->toJson(JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $person=Person::create([
+            'person_name'=>$request->person_name,
+            'users_id'=>$request->users_id
+        ]);
+
+        return $person->toJson(JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Person $person)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Person $person)
+    {
+        $person->update($request->all());
+        return $person->toJson(JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Person $person)
+    {
+        $person->delete();
+        return response('deleted',221);
+    }
+}
